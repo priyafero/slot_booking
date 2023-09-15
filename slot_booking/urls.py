5d4a327572c4ff5c6291ace34651f74b80862242
devalfero <deval@fero.ai>
2023-09-15 16:01:27 +0530

    added apis

--- slot_booking/urls.py
+++ slot_booking/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 slot_booking_router = routers.DefaultRouter()
 slot_booking_router.register(r'customers', views.CustomerViewSet)
+slot_booking_router.register(r'driver', views.DriverViewSet)
+slot_booking_router.register(r'vehicle', views.VehicleViewSet)
+slot_booking_router.register(r'warehouse', views.WarehouseViewSet)
+slot_booking_router.register(r'time_slot', views.TimeslotViewSet)
+slot_booking_router.register(r'driver_time_slot', views.DriverTimeslotAssignmentViewSet)
+slot_booking_router.register(r'order', views.OrderViewSet)
+slot_booking_router.register(r'trip', views.TripViewSet)
 # Add similar lines for other models
 
 urlpatterns = [
